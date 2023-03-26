9659d8196db024b344545ca1d9fc6b920401ebeb
Texterly <88582978+Texterly@users.noreply.github.com>
2023-03-26 23:34:36 +0300

    fixed exercises v4

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -17,7 +17,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
         justifyContent="center"
       >
         {exercises.map((exercise, index) => (
-          <p>{exercise.name}name of exercise</p>
+          <p>{exercise.name}</p>
         ))}
       </Stack>
     </Box>
